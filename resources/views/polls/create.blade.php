8dcae8c2a4898bb122a1f8578d513ba6543f8f3a
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-02-16 01:01:03 +0100

    --wip-- [skip ci]

--- resources/views/polls/create.blade.php
+++ resources/views/polls/create.blade.php
@@ -86,36 +86,85 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-6 col-sm-12 col-12 layout-spacing">
+                                        <div class="statbox widget box box-shadow">
+                                            <div class="widget-header">
+                                                <div class="row">
+                                                    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                                        <h4>If visibility is Private, Enter emails of allowed voters <span style="color: red">(Select checkbox if invite link should be sent.)</span></h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-12 col-md-6 col-sm-6 col-12 layout-spacing">
+                                                    <div class="statbox widget box box-shadow">
 
+                                                        <div class="widget-content widget-content-area">
+                                                            <div class="input-group mb-4">
+                                                                <textarea name="allowed_voters" id="" cols="90" rows="5" placeholder="E.g abc@def.com , xyz@kpk.com"></textarea>
+                                                                <div class="input-group-append">
+                                                                    <div class="input-group-text">
+                                                                        <label class="switch s-secondary mt-2">
+                                                                            <input type="checkbox" name="send_invite">
+                                                                            <span class="slider"></span><br>
+                                                                        </label>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-                                <div class="form-group mb-4">
-                                    <label for="inputAddress">Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="inputAddress2">Address 2</label>
-                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                                </div>
-                                <div class="form-row mb-4">
-                                    <div class="form-group col-md-6">
-                                        <label for="inputCity">City</label>
-                                        <input type="text" class="form-control" id="inputCity">
-                                    </div>
-
-                                    <div class="form-group col-md-2">
-                                        <label for="inputZip">Zip</label>
-                                        <input type="text" class="form-control" id="inputZip">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-check pl-0">
-                                        <div class="custom-control custom-checkbox checkbox-info">
-                                            <input type="checkbox" class="custom-control-input" id="gridCheck">
-                                            <label class="custom-control-label" for="gridCheck">Check me out</label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            <button type="submit" class="btn btn-button-7 mb-4 mt-3">Sign in</button>
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+                                        <div class="statbox widget box box-shadow">
+                                            <div class="widget-header">
+                                                <div class="row">
+                                                    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                                        <h4>Start Date - End Date</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="widget-content widget-content-area text-center">
+                                                <div class="row justify-content-between">
+                                                    <div class="col-lg-3 col-md-3 col-sm-4 col-6">
+                                                        <input type="date" name="start_date">
+                                                    </div>
+                                                    <div class="col-lg-3 col-md-3 col-sm-4 col-6">
+                                                        <input type="date" name="end_date">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+                                        <div class="statbox widget box box-shadow">
+                                            <div class="widget-header">
+                                                <div class="row">
+                                                    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                                        <h4>Start Time - End Time</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="widget-content widget-content-area text-center">
+                                                <div class="row justify-content-between">
+                                                    <div class="col-lg-3 col-md-3 col-sm-4 col-6">
+                                                        <input type="time" name="start_time">
+                                                    </div>
+                                                    <div class="col-lg-3 col-md-3 col-sm-4 col-6">
+                                                        <input type="time" name="end_time">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-button-7 mb-4 mt-3">Create</button>
                             </form>
                         </div>
                     </div>
