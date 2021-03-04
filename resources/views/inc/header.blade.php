952610f4821324c59ff2897b2135a9313637f875
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-03-04 15:53:48 +0100

    --wip-- [skip ci]

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -22,66 +22,8 @@
 </div>
 <header class="header navbar fixed-top navbar-expand-sm">
     <a href="javascript:void(0);" class="sidebarCollapse d-none d-lg-block" data-placement="bottom"><i class="flaticon-menu-line-2"></i></a>
-    <ul class="navbar-nav flex-row">
-        <li class="nav-item dropdown language-dropdown ml-1  ml-lg-0">
-            <a href="javascript:void(0);" class="nav-link dropdown-toggle" id="flagDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="assets/img/ca.svg" alt=""> <span class="d-lg-inline-block d-none"></span>
-            </a>
-            <div class="dropdown-menu position-absolute" aria-labelledby="flagDropdown">
-                <a class="dropdown-item" href="javascript:void(0);"><img src="assets/img/de.svg" class="flag-width" alt=""> &#xA0;German</a>
-                <a class="dropdown-item" href="javascript:void(0);"><img src="assets/img/jp.svg" class="flag-width" alt=""> &#xA0;Japanese</a>
-                <a class="dropdown-item" href="javascript:void(0);"><img src="assets/img/fr.svg" class="flag-width" alt=""> &#xA0;French</a>
-                <a class="dropdown-item" href="javascript:void(0);"><img src="assets/img/ru.svg" class="flag-width" alt=""> &#xA0;Russian</a>
-                <a class="dropdown-item" href="javascript:void(0);"><img src="assets/img/ca.svg" class="flag-width" alt=""> &#xA0;English</a>
-            </div>
-        </li>
-    </ul>
-
 
     <ul class="navbar-nav flex-row ml-lg-auto">
-
-        <li class="nav-item  d-lg-block d-none">
-            <form class="form-inline" role="search">
-                <input type="text" class="form-control search-form-control" placeholder="Search...">
-            </form>
-        </li>
-
-        <li class="nav-item dropdown app-dropdown  ml-lg-4 mr-lg-2 order-lg-0 order-2">
-            <a href="javascript:void(0);" class="nav-link dropdown-toggle" id="appDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="flaticon-bulb"></span>
-            </a>
-            <div class="dropdown-menu  position-absolute" aria-labelledby="appDropdown">
-                <a class="dropdown-item" href="ecommerce_dashboard.html">
-                    <i class="flaticon-commerce"></i><span>eCommerce</span>
-                </a>
-                <a class="dropdown-item" href="form_bootstrap_basic.html">
-                    <i class="flaticon-edit-3"></i><span>Forms</span>
-                </a>
-                <a class="dropdown-item" href="table_basic.html">
-                    <i class="flaticon-table"></i><span>Tables</span>
-                </a>
-                <a class="dropdown-item" href="ui_buttons.html">
-                    <i class="flaticon-switch"></i><span>Buttons</span>
-                </a>
-                <a class="dropdown-item" href="ui_flaticon_icon.html">
-                    <i class="flaticon-edit"></i><span>Font Icons</span>
-                </a>
-                <a class="dropdown-item" href="modules_widgets.html">
-                    <i class="flaticon-computer-5"></i><span>Modules</span>
-                </a>
-                <a class="dropdown-item" href="map_google_basic.html">
-                    <i class="flaticon-location"></i><span>Maps</span>
-                </a>
-                <a class="dropdown-item" href="apps_drag_n_drop_calendar.html">
-                    <i class="flaticon-calendar-1"></i><span>Calendar</span>
-                </a>
-                <a class="dropdown-item" href="am_column_and_barchart.html">
-                    <i class="flaticon-chart-3"></i><span>Charts</span>
-                </a>
-            </div>
-        </li>
-
-
         <li class="nav-item dropdown user-profile-dropdown ml-lg-0 mr-lg-2 ml-3 order-lg-0 order-1">
             <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="flaticon-user-12"></span>
