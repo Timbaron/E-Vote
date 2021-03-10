0059b57274af9b77bfb2e1bca55b6d67cab3f477
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-03-10 15:33:36 +0100

    Set Password Nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('avatar_original')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
