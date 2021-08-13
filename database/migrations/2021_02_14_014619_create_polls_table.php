f0e10f5774f8f1b4ff1652dc7880dd5216bbf2dd
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-08-13 13:16:17 +0100

    Remove poll id from poll table

--- database/migrations/2021_02_14_014619_create_polls_table.php
+++ database/migrations/2021_02_14_014619_create_polls_table.php
@@ -17,7 +17,6 @@ class CreatePollsTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('poll_id')->unique();
             $table->string('position');
             $table->string('allowed_voters')->nullable();
             $table->boolean('visibility');
