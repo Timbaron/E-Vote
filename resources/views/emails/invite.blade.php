9c7afea9a7765bd1f68327990858628139ea64e3
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-08-16 16:57:02 +0100

    Update invite blade

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Welcome to B-Vote
 You've been Inivted to a partisipate in a private poll, <br>
-<!-- <a href="">Click to vote</a> -->
+<!-- <a href="">Click to vote</a>    -->
 @component('mail::button', ['url' => '{{env('APP_URL').'/poll/cast/'}}'])
 Click here to vote
 @endcomponent
